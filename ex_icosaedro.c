c08fb11ae93d7fb99a8c6d06b00eb76b7b34eb9f
drmcga <64701716+drmcga@users.noreply.github.com>
2023-04-18 18:47:39 -0300

    Update ex_icosaedro.c

--- ex_icosaedro.c
+++ ex_icosaedro.c
@@ -1,16 +1,14 @@
 /*
 ENUNCIADO
-
     Desenvolva um analisador de lancamento de 6 Icosaedros (dados de 20 lados)
     que forneca os lancamentos que contenham apenas numeros primos, desconsiderando repeticoes.
-
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 //ve se o numero inserido e primo
-int is_primo(valor_n){
+int is_primo(int valor_n){
 
     if (valor_n==1) return 0;
 
@@ -27,16 +25,46 @@ int is_primo(valor_n){
 int main()
 {
 
+    int cont=0;
+
+    /*     //bloco de teste de primos
     for (int i=1; i<=20;i++){
         printf("%d : %d\n",i,is_primo(i));
     }
+    */
 
+    printf("  Todas as possibilidades de lancamentos para 6 Icosaedros,\n em que todos os valores obtidos sao primos e distintos entre si:\n\n[");
+    for (int i=1; i<=20; i++){
+        for (int j=i+1; j<=20; j++){
+            for (int m=j+1; m<=20; m++){
+                for (int n=m+1; n<=20; n++){
+                    for (int r=n+1; r<=20; r++){
+                        for (int s=r+1; s<=20; s++){
+
+                            if(is_primo(i) && is_primo(j) &&
+                               is_primo(m) && is_primo(n) &&
+                               is_primo(r) && is_primo(s)) {
+
+                                if ((cont%4)==0 && cont!=0){
+                                    printf("\n\t");
+                                }else {
+                                    printf("\t");
+                                }
+
+                                printf("%2d %2d %2d %2d %2d %2d,",i,j,m,n,r,s);
+                                cont++;
+                                }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    printf("\b ]\n");
 
 
 
 
     return 0;
 }
-
-
-
